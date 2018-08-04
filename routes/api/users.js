70c2627758eb8621369df63828db95db063fee93
egan <reagankganancial@gmail.com>
2018-08-04 15:36:10 +0800

    3rd commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // Load User Model
-const User = require("../../models/User");
+const User = require('../../models/User');
 
 // @route GET api/users/test
 // @desc Test users route
 // @access Public
-router.get("/test", (req, res) => res.json({ msg: "User Page" }));
+router.get('/test', (req, res) => res.json({ msg: 'User Page' }));
 
 // @route GET api/users/register
 // @desc Register users
 // @access Public
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      return res.status(400).json({ email: 'Email already exists' });
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: "200", // Size
-        r: "pg", // Rating
-        d: "mm" // Default
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
       });
       const newUser = new User({
         name: req.body.name,
@@ -41,6 +41,30 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route GET api/users/login
+// @desc Login Users / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({email}).then(user => {
+    // Check for user
+    if(!user) {
+      return res.status(404).json({email: 'User not found'});
+    }
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if(isMatch) {
+        res.json({msg: 'Success'});
+      } else {
+        return res.status(400).json({password: 'Password incorrect'});
+      }
+    })
   });
 });
 
